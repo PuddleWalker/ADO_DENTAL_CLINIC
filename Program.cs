be1d6d2eebb17effac89489240f9772a3077301c
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2025-05-22 22:38:52 +0300

    SQLite

--- Program.cs
+++ Program.cs
@@ -1,58 +1,17 @@
-﻿using Microsoft.Data.SqlClient;
-using System.Configuration;
-using System.Data;
+﻿using Microsoft.Data.Sqlite;
+//using System.Configuration;
 
-string connectionString = ConfigurationManager.ConnectionStrings["DENTAL_CLINIC"].ToString();
+//string conString = ConfigurationManager.ConnectionStrings["DENTAL_CLINIC"].ToString();
+string conString = "Data Source = DENTAL_CLINIC.db";
 
+string query = "select Name, Surname, City, Country from Dental_clinic as DC, Dentist as D " +
+                       "where DC.Country like 'Russia%' and DC.DentistID = D.id";
 
-using SqlConnection connection = new(connectionString);
-
-const string queryString = "SELECT * from Dentist";
-/*
-const string queryString = "SELECT e.Tab, e.SecondName, e.FirstName, e.CountryId, e.RegionId, c.Name, r.Name " +
-                           "from employee e, country c, region r " +
-                           "where e.CountryId = c.id and e.RegionCode = r.Code;";
-*/
-SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
-DataSet Dentists = new DataSet("Dentist");
-adapter.Fill(Dentists, "Dentist");
-// можно и не делать - сразу обратиться
-DataTable? dt = Dentists.Tables["Dentist"];
-
-// добавляем строку
-DataRow newRow = dt.NewRow();
-newRow["id"] = 70;
-newRow["Name"] = "Александр";
-newRow["Surname"] = "Петров";
-newRow["CategoryID"] = 3;
-newRow["SpecialityID"] = 2;
-newRow["BeginDate"] = "14.03.2008";
-dt.Rows.Add(newRow);
-// изменяем значение
-dt.Rows[0]["SpecialityID"] = 1;
-// тут магия - делаем Builder, но нигде его не задаем - она сам дает Update, Insert или Delete
-// но только для простых select (без условий и т.д.)
-SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
-// обновляем источник - БД
-adapter.Update(Dentists, "Dentist");
-// очищаем полностью DataSet
-Dentists.Clear();
-// перезагружаем данные
-adapter.Fill(Dentists, "Dentist");
-
-try
+using SqliteConnection connection = new(conString);
+SqliteCommand c = new(query, connection);
+connection.Open();
+using SqliteDataReader reader = c.ExecuteReader();
+while (reader.Read())
 {
-    // обращаемся по имени таблицы в коллекции Tables
-    foreach (DataRow row in dt.Rows)
-    {
-        foreach (var cell in row.ItemArray)
-        {
-            Console.Write($"{cell}\t");
-        }
-        Console.WriteLine();
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
+    Console.WriteLine("{0}{1}\t{2}\t{3}", reader["Name"], reader["Surname"], reader[2], reader[3]);
 }
