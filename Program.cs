f837e1d606cc98b59bd3d002e27eab05f149280a
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2025-05-22 18:15:19 +0300

    DataSet

--- Program.cs
+++ Program.cs
@@ -1,31 +1,56 @@
 ﻿using Microsoft.Data.SqlClient;
 using System.Configuration;
 using System.Data;
-using static System.Console;
 
 string connectionString = ConfigurationManager.ConnectionStrings["DENTAL_CLINIC"].ToString();
 
 
-Write("Введите название страны: ");
-string? reg = ReadLine();
-// string? reg = "моск"; // для проверки - не чувствителен SQL у нас к регистру по умолчанию
-SqlParameter regParam = new("@reg", "%" + reg + "%");
-const string queryString = "SELECT DC.City from Dental_clinic DC " +
-                           "where DC.Country LIKE @reg";
-WriteLine(queryString);
 using SqlConnection connection = new(connectionString);
-SqlCommand command = new(queryString, connection);
-command.Parameters.Add(regParam);
+
+const string queryString = "SELECT * from Dentist";
+/*
+const string queryString = "SELECT e.Tab, e.SecondName, e.FirstName, e.CountryId, e.RegionId, c.Name, r.Name " +
+                           "from employee e, country c, region r " +
+                           "where e.CountryId = c.id and e.RegionCode = r.Code;";
+*/
+SqlDataAdapter adapter = new SqlDataAdapter(queryString, connection);
+DataSet Dentists = new DataSet("Dentist");
+adapter.Fill(Dentists, "Dentist");
+// можно и не делать - сразу обратиться
+DataTable? dt = Dentists.Tables["Dentist"];
+
+// добавляем строку
+DataRow newRow = dt.NewRow();
+newRow["id"] = 70;
+newRow["Name"] = "Александр";
+newRow["Surname"] = "Петров";
+newRow["CategoryID"] = 3;
+newRow["SpecialityID"] = 2;
+newRow["BeginDate"] = "14.03.2008";
+dt.Rows.Add(newRow);
+// изменяем значение
+dt.Rows[0]["SpecialityID"] = 1;
+// тут магия - делаем Builder, но нигде его не задаем - она сам дает Update, Insert или Delete
+// но только для простых select (без условий и т.д.)
+SqlCommandBuilder commandBuilder = new SqlCommandBuilder(adapter);
+// обновляем источник - БД
+adapter.Update(Dentists, "Dentist");
+// очищаем полностью DataSet
+Dentists.Clear();
+// перезагружаем данные
+adapter.Fill(Dentists, "Dentist");
+
 try
 {
-    connection.Open();
-    var reader = command.ExecuteReader();
-    while (reader.Read())
+    // обращаемся по имени таблицы в коллекции Tables
+    foreach (DataRow row in dt.Rows)
     {
-        WriteLine(reader[0]);
+        foreach (var cell in row.ItemArray)
+        {
+            Console.Write($"{cell}\t");
+        }
+        Console.WriteLine();
     }
-    reader.Close();
-    connection.Close(); // в принципе не надо, т.к. у нас using
 }
 catch (Exception ex)
 {
