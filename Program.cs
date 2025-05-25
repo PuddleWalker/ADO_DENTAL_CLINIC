3950789775e457ad24286c08073cf51b899bd153
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2025-05-25 18:24:49 +0300

    Фабрика провайдеров

--- Program.cs
+++ Program.cs
@@ -1,17 +1,48 @@
-﻿using Microsoft.Data.Sqlite;
-//using System.Configuration;
+﻿using System.Configuration;
+using System.Data;
+using System.Data.Common;
+using Microsoft.Data.SqlClient;
+using Microsoft.Data.Sqlite;
+using static System.Console;
 
-//string conString = ConfigurationManager.ConnectionStrings["DENTAL_CLINIC"].ToString();
-string conString = "Data Source = DENTAL_CLINIC.db";
+WriteLine("\n  Выберите базу данных: ");
+WriteLine("  1. SQL Server Express");
+WriteLine("  2. SQLite");
+Write("  ");
+int choice = Convert.ToInt32(ReadLine());
+DbProviderFactory? factory = null;
+string cs;
+switch (choice)
+{
+    case 1:
+        factory = SqlClientFactory.Instance;
+        cs = ConfigurationManager.ConnectionStrings["DENTAL_CLINIC"].ToString();
+        break;
+    case 2:
+        factory = SqliteFactory.Instance;
+        cs = ConfigurationManager.ConnectionStrings["DENTAL_SQLite"].ConnectionString;
+        break;
+    default:
+        Console.WriteLine("  Некорректный выбор");
+        return;
+}
+// создаем соединение и назначаем ему строку подключения
+DbConnection? con = factory.CreateConnection();
+con.ConnectionString = cs;
 
-string query = "select Name, Surname, City, Country from Dental_clinic as DC, Dentist as D " +
-                       "where DC.Country like 'Russia%' and DC.DentistID = D.id";
+DbCommand cmd = con.CreateCommand();
+cmd.CommandText = "select Name, Surname, City, Country from Dental_clinic as DC, Dentist as D " +
+                                   "where DC.Country like 'Russia%' and DC.DentistID = D.id";
 
-using SqliteConnection connection = new(conString);
-SqliteCommand c = new(query, connection);
-connection.Open();
-using SqliteDataReader reader = c.ExecuteReader();
+con.Open();
+
+using DbDataReader reader = cmd.ExecuteReader();
 while (reader.Read())
 {
     Console.WriteLine("{0}{1}\t{2}\t{3}", reader["Name"], reader["Surname"], reader[2], reader[3]);
 }
+
+WriteLine("  Нажмите любую клавишу...");
+ReadKey();
+
+con.Close();
