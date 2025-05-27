88cf5f2e9c67a6308a69a658de360f936c7ed4c4
Тимур Миндубаев <mindubaev.timurka@gmail.com>
2025-05-27 19:40:38 +0300

    Fluent API

--- Program.cs
+++ Program.cs
@@ -1,53 +1,155 @@
-﻿using Microsoft.EntityFrameworkCore;
-using System.Collections.Generic;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
 using static System.Console;
-using System.Configuration;
 
-using (ApplicationContext db = new ApplicationContext())
+WriteLine("Спасибо Марку Прайсу " +
+          "cs13net9-main\\docs\\ch10-code-first");
+
+using (Clinic c = new())
 {
-    // получаем объекты из бд и выводим на консоль
-    var dentists = db.Dentist.ToList();
-    WriteLine("Список объектов из БД:");
-    foreach (Dentist d in dentists)
+    bool deleted = await c.Database.EnsureDeletedAsync();
+    WriteLine($"\nБаза данных Clinic удалена: {deleted}");
+
+    bool created = await c.Database.EnsureCreatedAsync();
+    WriteLine($"\nБаза данных Clinic создана: {created}");
+
+    WriteLine("\nSQL скрипт для создания БД:");
+    WriteLine(c.Database.GenerateCreateScript());
+
+    WriteLine("\nДоктора:");
+    foreach (Doctor d in c.Doctors.Include(d => d.Speciality))
     {
-        Console.WriteLine($"{d.Id} - {d.Name?.Trim()} {d.Surname?.Trim()}");
-    }
-
-    // создаем два новых объекта User
-    WriteLine("Добавляем новых...");
-    Dentist andrew = new Dentist { Name = "Андрей", Surname = "Быков", SpecialityID = 4, CategoryID = 1, BeginDate = new DateTime(2002, 1, 13) };
-    Dentist dmitriy = new Dentist { Name = "Дмитрий", Surname = "Старцев", SpecialityID = 1, CategoryID = 2, BeginDate = new DateTime(2008, 12, 30) };
-
-    // добавляем их в бд
-    db.Dentist.Add(andrew);
-    db.Dentist.Add(dmitriy);
-    db.SaveChanges();
-    WriteLine("Объекты успешно сохранены");
-
-    // снова получаем объекты из бд и выводим на консоль
-    dentists = db.Dentist.ToList();
-    Console.WriteLine("Список объектов:");
-    foreach (Dentist d in dentists)
-    {
-        Console.WriteLine($"{d.Id} - {d.Name?.Trim()} {d.Surname?.Trim()}");
+        WriteLine("{0} {1} является доктором со специальностью {2}",
+          d.FirstName, d.SecondName, d.Speciality.Name);
     }
 }
-public class Dentist
+public class Clinic : DbContext
 {
-    public int Id { get; set; } 
+    public DbSet<Doctor>? Doctors { get; set; }
+    public DbSet<Speciality>? Specialities { get; set; }
+    public DbSet<Category>? Categories { get; set; }
+    protected override void OnConfiguring(
+      DbContextOptionsBuilder optionsBuilder)
+    {
+        string connection = "Data Source=AstraCore;Initial Catalog=Clinic;" +
+                            "Integrated Security=true;Encrypt=False";
+        optionsBuilder.UseSqlServer(connection);
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Fluent API validation rules
+        modelBuilder.Entity<Doctor>()
+          .Property(d => d.SecondName).HasMaxLength(30).IsRequired();
+        // populate database with sample data
+        Doctor a = new()
+        {
+            id = 1,
+            FirstName = "Алексей",
+            SecondName = "Политехнический",
+            CategoryID = 4,
+            SpecialityID = 2
+        };
+        Doctor b = new()
+        {
+            id = 2,
+            FirstName = "Вениамин",
+            SecondName = "Вересаев",
+            CategoryID = 1,
+            SpecialityID = 3
+        };
+        Doctor c = new()
+        {
+            id = 3,
+            FirstName = "Джон",
+            SecondName = "Хизклифф",
+            CategoryID = 2,
+            SpecialityID = 3
+        };
+        Category s = new()
+        {
+            id = 1,
+            Name = "Врач-стажёр"
+        };
+        Category o = new()
+        {
+            id = 2,
+            Name = "Врач-ординатор"
+        };
+        Category f = new()
+        {
+            id = 3,
+            Name = "Врач первой степени"
+        };
+        Category u = new()
+        {
+            id = 4,
+            Name = "Врач высшей категории"
+        };
+        Speciality k = new()
+        {
+            id = 1,
+            Name = "Кардиолог"
+        };
+        Speciality d = new()
+        {
+            id = 2,
+            Name = "Дерматовенеролог"
+        };
+        Speciality g = new()
+        {
+            id = 3,
+            Name = "Гастроэнтеролог"
+        };
+        Speciality n = new()
+        {
+            id = 4,
+            Name = "Нейрохирург"
+        };
+
+        modelBuilder.Entity<Doctor>()
+          .HasData(a, b, c);
+
+        modelBuilder.Entity<Category>()
+          .HasData(s, o, f, u);
+
+        modelBuilder.Entity<Speciality>()
+          .HasData(k, d, g, n);
+
+
+        modelBuilder.Entity<Doctor>()
+            .HasOne(d => d.Category)
+            .WithMany(c => c.Doctors)
+            .HasForeignKey(d => d.CategoryID);
+
+        modelBuilder.Entity<Doctor>()
+            .HasOne(d => d.Speciality)
+            .WithMany(s => s.Doctors)
+            .HasForeignKey(d => d.SpecialityID);
+    }
+}
+public class Speciality
+{
+    public int id { get; set; }
     public string? Name { get; set; }
-    public string? Surname { get; set; }
+    public ICollection<Doctor>? Doctors { get; set; }
+}
+public class Category
+{
+    public int id { get; set; }
+    public string? Name { get; set; }
+    public ICollection<Doctor>? Doctors { get; set; }
+}
+public class Doctor
+{
+    public int id { get; set; }
+
     public int CategoryID { get; set; }
     public int SpecialityID { get; set; }
-    public DateTime? BeginDate { get; set; }
-}
-public class ApplicationContext : DbContext
-{
-    string connectionString = ConfigurationManager.ConnectionStrings["DENTAL_First"].ToString();
-    public DbSet<Dentist> Dentist => Set<Dentist>();
-    public ApplicationContext() => Database.EnsureCreated();
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        optionsBuilder.UseSqlServer(connectionString);
-    }
+
+    public string? FirstName { get; set; }
+    public string? SecondName { get; set; }
+
+
+    public Category? Category { get; set; }
+    public Speciality? Speciality { get; set; }
 }
